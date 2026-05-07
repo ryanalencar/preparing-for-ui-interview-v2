63103ad19060e953374d9db07208cdf9e808bfa0
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2026-05-07 13:55:42 -0300

    chore: refactor myExtends function and restore example usage for clarity

--- src/problems/06-es5-extends/es5-extends.vanila.ts
+++ src/problems/06-es5-extends/es5-extends.vanila.ts
@@ -2,26 +2,35 @@
 
 export const myExtends = (SuperType: Function, SubType: Function) => {
   // Step 1: Create a new constructor function MyType(this, ...args)
+  function ExtendedType(...args: any[]) {
+    const target = Object.create(SubType.prototype)
+    SuperType.apply(target, args)
+    SubType.apply(target, args)
+    return target
+  }
   // Step 2: Set up prototype chain
+  Object.setPrototypeOf(SubType.prototype, SuperType.prototype)
   // Step 3: Set up static/constructor inheritance
+  Object.setPrototypeOf(ExtendedType, SuperType)
   // Step 4: Return MyType
+  return ExtendedType
 }
 
 // --- Examples ---
 // Uncomment to test your implementation:
 
-// function Animal(this: any, name: string) { this.name = name }
-// Animal.print = () => { console.log('Animal') }
-// Animal.prototype.greet = function () { return `Hello, ${this.name}` }
-//
-// function Dog(this: any) { this.breed = 'Labrador' }
-// Dog.prototype.bark = function () { return `${this.name} says Woof!` }
-//
-// const DogExtended = myExtends(Animal, Dog)
-// const dog = new (DogExtended as any)('Rex')
-// console.log(dog.name)    // Expected: "Rex"
-// console.log(dog.breed)   // Expected: "Labrador"
-// console.log(dog.greet()) // Expected: "Hello, Rex"
-// console.log(dog.bark())  // Expected: "Rex says Woof!"
-// console.log(dog instanceof Animal) // Expected: true
-// (DogExtended as any).print() // Expected: "Animal"
+function Animal(this: any, name: string) { this.name = name }
+Animal.print = () => { console.log('Animal') }
+Animal.prototype.greet = function () { return `Hello, ${this.name}` }
+
+function Dog(this: any) { this.breed = 'Labrador' }
+Dog.prototype.bark = function () { return `${this.name} says Woof!` }
+
+const DogExtended = myExtends(Animal, Dog)
+const dog = new (DogExtended as any)('Rex')
+console.log(dog.name)    // Expected: "Rex"
+console.log(dog.breed)   // Expected: "Labrador"
+console.log(dog.greet()) // Expected: "Hello, Rex"
+console.log(dog.bark())  // Expected: "Rex says Woof!"
+console.log(dog instanceof Animal) // Expected: true
+(DogExtended as any).print() // Expected: "Animal"
