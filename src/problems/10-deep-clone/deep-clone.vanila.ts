e16420c010e480f8d645ca5c6749999c31a79dad
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2026-05-08 14:22:55 -0300

    chore: implement deepClone funcion

--- src/problems/10-deep-clone/deep-clone.vanila.ts
+++ src/problems/10-deep-clone/deep-clone.vanila.ts
@@ -5,9 +5,39 @@ import { detectType } from '@course/utils'
 
 type TCollection = Map<any, any> | Set<any> | Record<any, any> | Array<any>
 
-function getTarget(type: string): TCollection {}
+function getTarget(type: string): TCollection {
+  switch (type) {
+    case 'map':
+      return new Map()
+    case 'set':
+      return new Set()
+    case 'array':
+      return []
+    case 'object':
+      return {}
+    default:
+      throw new Error(`Unsupported type: ${type}`)
+  }
+}
 function entries(target: TCollection): Iterable<[key: any, value: any]> {
-function set(target: TCollection, key: any, value: any) {}
+  if (target instanceof Map || target instanceof Array || target instanceof Set) {
+    return target.entries()
+  } else {
+    return Object.entries(target)
+  }
+}
+
+function set(target: TCollection, key: any, value: any) {
+  if (target instanceof Map) {
+    target.set(key, value)
+  } else if (target instanceof Set) {
+    target.add(value)
+  } else if (target instanceof Array) {
+    target[key as number] = value
+  } else {
+    target[key] = value
+  }
+}
 
 export const deepClone = <T>(a: T, cache = new Map()): T => {
   const type = detectType(a)
@@ -18,10 +48,21 @@ export const deepClone = <T>(a: T, cache = new Map()): T => {
 
   switch (type) {
     case 'date':
+      return new Date(a as unknown as Date) as T
     case 'object':
     case 'map':
     case 'set':
-    case 'array':
+    case 'array': {
+      if (cache.has(a)) {
+        return cache.get(a)
+      }
+      const target = getTarget(type)
+      cache.set(a, target)
+      for (const [key, value] of entries(a)) {
+        set(target, key, deepClone(value, cache))
+      }
+      return target as T
+    }
     default:
       throw 'Unsupported type ' + a
   }
@@ -33,14 +74,15 @@ export const deepClone = <T>(a: T, cache = new Map()): T => {
 // const obj = { a: { b: 1 }, c: [2, 3] }
 // const cloned = deepClone(obj)
 // cloned.a.b = 99
-// console.log(obj.a.b)     // Expected: 1 (unaffected)
-// console.log(cloned.a.b)  // Expected: 99
+// console.log(obj.a.b) // Expected: 1 (unaffected)
+// console.log(cloned.a.b) // Expected: 99
 //
 // const map = new Map([['key', { value: 1 }]])
 // const clonedMap = deepClone(map)
 // console.log(clonedMap.get('key'))  // Expected: { value: 1 }
 // console.log(clonedMap.get('key') !== map.get('key'))  // Expected: true
 //
-// const circular: any = { a: 1 }; circular.self = circular
-// const clonedCircular = deepClone(circular)
-// console.log(clonedCircular.self === clonedCircular)  // Expected: true
+const circular: any = { a: 1 }
+circular.self = circular
+const clonedCircular = deepClone(circular)
+console.log(clonedCircular.self === clonedCircular) // Expected: true
