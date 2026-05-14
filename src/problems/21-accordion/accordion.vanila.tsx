2236ed9c5fa7fc8a146789d8379612347ad81554
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2026-05-14 10:06:01 -0300

    chore: enhance Accordion component with HTML rendering and item template logic

--- src/problems/21-accordion/accordion.vanila.tsx
+++ src/problems/21-accordion/accordion.vanila.tsx
@@ -29,7 +29,19 @@ type TAccordionProps = {
 }
 
 export class Accordion extends AbstractComponent<TAccordionProps> {
+  constructor(config: TComponentConfig<TAccordionProps>) {
+    super(config)
+  }
+
   toHTML(): string {
-    return ``
+    const content = this.config.items.map(this.getItemTemplate).join('')
+    return `<ul>${content}</ul>`
+  }
+
+  getItemTemplate = (item: TAccordionItem) => {
+    return `<li><details class="${styles.details}">
+    <summary class="${styles.summary}">${item.title}</summary>
+    <section>${item.content}</section>
+    </details></li>`
   }
 }
