4efabc7bb7dc38740eb523a2ff7b108e60d6f8c9
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2026-06-08 14:28:34 -0300

    chore: implement MyParameters type to infer function parameter types

--- src/problems/31-parameters-challenge-15/31-parameters-challenge-15.ts
+++ src/problems/31-parameters-challenge-15/31-parameters-challenge-15.ts
@@ -13,7 +13,9 @@ import type { Equal, Expect } from '@course/types'
 
 /* _____________ Your Code Here _____________ */
 
-type MyParameters = {};
+type MyParameters<F extends (...args: any[]) => any> = F extends (...args: infer Params) => any
+  ? Params
+  : never
 
 /* _____________ Test Cases _____________ */
 
@@ -21,8 +23,10 @@ function foo(arg1: string, arg2: number): void {}
 function bar(arg1: boolean, arg2: { a: 'A' }): void {}
 function baz(): void {}
 
+
 type cases = [
   Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
   Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
   Expect<Equal<MyParameters<typeof baz>, []>>,
+  Expect<Equal<Parameters<typeof baz>, []>>,
 ]
